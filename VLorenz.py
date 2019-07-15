99d0743e989134fa8ec10d8eb0d14d17816920f1
Daudi Wampamba <52892400+dthelegend@users.noreply.github.com>
2019-07-15 01:50:58 +0100

    Update VLorenz.py

--- VLorenz.py
+++ VLorenz.py
@@ -6,10 +6,12 @@ from random import randint as r, seed as seed
 #Using a random seed to make sure wheel generation is always the same
 seed(75434390)
 
-mode = 0
+#Indicates State of Figure Shift/Letter Shift
+Omode = 0
+Imode = 0
 
-class TwoWayDict(dict):
-    def __missing__(self, key):
+class TwoWayDict(dict): #What it says on the box
+    def __missing__(self, key): #In case the box wasn't good enough, this creates exceptions that allow the values to be used as keys that map to their respective keys
         answer = None
         try:
             for i in self:
@@ -28,12 +30,12 @@ class TwoWayDict(dict):
             #raise KeyError("No matching values found.")
             pass
 
-class specials(dict):
+class specials(dict): #Making my life easier to replace certain characters with symbols
     def __missing__(self, key):
         return key
     
 
-IT2 = TwoWayDict()
+IT2 = TwoWayDict() #A list of all the International Table 2 codes for the letters. Honestly this took way too long...
 IT2.update({
 ("#","#"):(0,0,0,0,0),
 ("BACKSPACE","BACKSPACE"):(0,1,0,0,0),
@@ -68,9 +70,10 @@ IT2.update({
 ("CAPS LOCK","CAPS LOCK"):(1,1,0,1,1),
 ("TAB","TAB"):(1,1,1,1,1)})
 
-Translate = specials({"TAB":"-","ENTER":("\n"),"CAPS LOCK":"|","SPACE":"_","BACKSPACE":"<-"})
+Translate = specials({"TAB":"-","ENTER":("\n"),"CAPS LOCK":"|","SPACE":"_","BACKSPACE":"<-"}) #The prodigal son returns
+Rich = specials({"SPACE":" ", "BACKSPACE":"\b","ENTER":"\n"})
 
-def xor(k,s):
+def xor(k,s): #eXclusive-OR function to perform modulo 2 addition
     
     if((int(k) == 0 or int(k) == 1) and (int(s) == 0 or int(s) == 1)):
         
@@ -86,14 +89,14 @@ def xor(k,s):
         
         raise TypeError("Only accepts binary 0 or 1 inputs")
 
-class wheel():
+class wheel(): #Defines the characteristics and behaviours of each of the wheels in the program
     
-    def __init__(self, numberOfPins, motor, pins = None):
+    def __init__(self, numberOfPins, motor = None, pins = None):
         
         self.pins = ([r(0,1) for _ in range(numberOfPins)] if(pins == None) else pins)
         self.pointer = 0
         self.motor = motor
-    
+        
     def __len__(self):
         
         return len(self.pins)
@@ -124,21 +127,24 @@ class wheel():
     
     def __str__(self):
         
-        return str(self.pins[self.pointer])
+        return f"Wheel with {len(self)} pins. Current position: {str(self.pins[self.pointer])}"
 
-def stepAll(array):
+def stepAll(array): #A function to step all the wheels in an array
     
     for g in array:
         
         g.step()
-    
+
+#Wheel Town
 chi = [wheel(41),wheel(31),wheel(29),wheel(26),wheel(23)]
 psi = [wheel(43),wheel(47),wheel(51),wheel(53),wheel(59)]
 mu =  [wheel(61),wheel(37)]
 
+#The transcription function in the flesh
 def live_transcribe(e):
     
-    global mode
+    global Imode
+    global Omode
     
     #print(e.name)
     
@@ -162,7 +168,7 @@ def live_transcribe(e):
         #print(IT2[str(e.name).upper()])
         for i in range(5):
             #print(IT2[str(e.name).upper()][i],chi[i],psi[i])
-            out += (xor(xor(IT2[translating][i],chi[i]),psi[i]),)
+            out += (xor(xor(IT2[translating][i],chi[i]),psi[i]),) #xorxorxorxor
             
         stepAll(chi)
         if(mu[0] == 1):
@@ -174,12 +180,28 @@ def live_transcribe(e):
         #print(("".join([str(item) for item in IT2[str(e.name).upper()]]) if(len(IT2[str(e.name).upper()]) > 2) else IT2[str(e.name).upper()][mode])+ " ")
         #print("".join([str(stuff) for stuff in list(out)]))
         
-        if(IT2[out][mode] == "CAPS LOCK"):
-            mode = 0
-        elif(IT2[out][mode] == "TAB"):
-            mode = 1
+        if(translating == "CAPS LOCK"):
+            Imode = 0
+        elif(translating == "TAB"):
+            Imode = 1
+        if(IT2[out][Omode] == "CAPS LOCK"):
+            Omode = 0
+        elif(IT2[out][Omode] == "TAB"):
+            Omode = 1
         
-        print(Translate[IT2[out][mode]], end = "")
+        #Outputs to the labels I have made
+        pre.config(text = (pre.cget("text") + (Translate[str(e.name).upper()])))
+        for a in list(IT2.keys()):
+            if(translating in a and (translating not in list(Translate.keys()) or (translating == "SPACE" or translating == "BACKSPACE" or translating == "ENTER"))):
+                if(translating == "BACKSPACE"):
+                    if(len(before.cget("text")) > 9):
+                        before.config(text = before.cget("text")[0:-1])
+                else:
+                    before.config(text = before.cget("text") + Rich[a[Imode]])
+        tran.config(text = (tran.cget("text") + str(Translate[IT2[out][Omode]])))
+        for b in list(IT2.keys()):
+            if(IT2[out][Omode] in b): #  and (IT2[out][Omode] not in list(Translate.keys()) or (IT2[out][Omode] == "SPACE" or IT2[out][Omode] == "BACKSPACE" or IT2[out][Omode] == "ENTER"))
+                after.config(text = after.cget("text") + Translate[b[Imode]])
         
     except:
         
@@ -188,16 +210,19 @@ def live_transcribe(e):
 
 #Keyboard Setup
 keyboard.on_press(live_transcribe)
-keyboard.block_key("BACKSPACE")
 
 #Tkinter Showcase Code
 master = Tk()
 master.geometry("800x300")
 sub = Frame(master)
-pre = Entry(sub, width = 100)
-tran = Label(sub,text = "")
+pre = Label(sub, text = "RAW IN: ")
+before = Label(sub, text = "RICH IN: ")
+tran = Label(sub,text = "RAW OUT: ")
+after = Label(sub,text = "RICH OUT: ")
 pre.pack()
+before.pack()
 tran.pack()
+after.pack()
 sub.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 master.mainloop()
 
@@ -208,5 +233,8 @@ TEST for numbers:
 GRY
 TEST for alphabet:
 FAH-)8!Bell20&275$9.')=5)WRU?WRU?://+_6+,8_!+_!1-<-!
+
+TEST for free:
+TXHGNBQEZO|S|QBCA
 
 """
